3cea52b668599eb867f30e8d943d09eb1e0c006e
Fernando Cabrera <fernando.cabrera@koombea.com>
2017-10-11 10:37:55 -0500

    add ports and start script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3001;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -54,8 +55,9 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
-app.listen(3001, () => {
-    console.log('Server is up on port 3001');
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
 
     //res.send('<h1>Felicitaciones !!!!</h1>');
